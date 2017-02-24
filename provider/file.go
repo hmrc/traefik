0ee77fa01c946622cd5078d16571a1f3122faefa
Richard Shepherd <richard.shepherd@digital.hmrc.gov.uk>
2017-02-24 12:31:05 +0000

    Added directory watcher and test

--- provider/file.go
+++ provider/file.go
@@ -2,7 +2,6 @@ package provider
 
 import (
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/BurntSushi/toml"
@@ -10,6 +9,10 @@ import (
 	"github.com/containous/traefik/safe"
 	"github.com/containous/traefik/types"
 	"gopkg.in/fsnotify.v1"
+	"io/ioutil"
+	"path"
+	"time"
+	"fmt"
 )
 
 var _ Provider = (*File)(nil)
@@ -17,11 +20,22 @@ var _ Provider = (*File)(nil)
 // File holds configurations of the File provider.
 type File struct {
 	BaseProvider `mapstructure:",squash"`
+	Directory string `description:"Read config from files in this directory"`
 }
 
 // Provide allows the provider to provide configurations to traefik
 // using the given configuration channel.
 func (provider *File) Provide(configurationChan chan<- types.ConfigMessage, pool *safe.Pool, constraints types.Constraints) error {
+	if provider.Directory != "" {
+		provider.handleDirectory(configurationChan, pool, constraints)
+	} else {
+		provider.handleSingleFile(configurationChan, pool, constraints)
+	}
+
+	return nil
+}
+
+func (provider *File) handleSingleFile(configurationChan chan<- types.ConfigMessage, pool *safe.Pool, constraints types.Constraints) error {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Error("Error creating file watcher", err)
@@ -39,46 +53,121 @@ func (provider *File) Provide(configurationChan chan<- types.ConfigMessage, pool
 		// Process events
 		pool.Go(func(stop chan bool) {
 			defer watcher.Close()
-			for {
-				select {
-				case <-stop:
-					return
-				case event := <-watcher.Events:
-					if strings.Contains(event.Name, file.Name()) {
-						log.Debug("File event:", event)
-						configuration := provider.loadFileConfig(file.Name())
-						if configuration != nil {
-							configurationChan <- types.ConfigMessage{
-								ProviderName:  "file",
-								Configuration: configuration,
-							}
-						}
-					}
-				case error := <-watcher.Errors:
-					log.Error("Watcher event error", error)
-				}
-			}
+			debounce(1*time.Second, watcher, stop, func(event fsnotify.Event) {
+				provider.loadFileConfig(file.Name(), configurationChan)
+			})
 		})
-		err = watcher.Add(filepath.Dir(file.Name()))
+		err = watcher.Add(file.Name())
 		if err != nil {
 			log.Error("Error adding file watcher", err)
 			return err
 		}
 	}
 
-	configuration := provider.loadFileConfig(file.Name())
+	provider.loadFileConfig(file.Name(), configurationChan)
+	return nil
+}
+
+func debounce(interval time.Duration, watcher *fsnotify.Watcher, stop chan bool, f func(arg fsnotify.Event)) {
+	var (
+		event    fsnotify.Event
+		eventptr *fsnotify.Event
+	)
+
+	for {
+		select {
+		case event = <-watcher.Events:
+			eventptr = &event
+		case <-time.After(interval):
+			if eventptr != nil {
+				f(event)
+				eventptr = nil
+			}
+		case <-stop:
+			return
+		case err := <-watcher.Errors:
+			log.Error("Watcher event error", err)
+		}
+	}
+}
+
+func (provider *File) handleDirectory(configurationChan chan<- types.ConfigMessage, pool *safe.Pool, constraints types.Constraints) error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Error("Error creating directory watcher", err)
+		return err
+	}
+
+	if provider.Watch {
+		// Process events
+		pool.Go(func(stop chan bool) {
+			defer watcher.Close()
+			debounce(5*time.Second, watcher, stop, func(event fsnotify.Event) {
+				provider.loadFileConfigFromDir(configurationChan)
+			})
+		})
+
+		err = watcher.Add(provider.Directory)
+		if err != nil {
+			log.Error("Error adding directory watcher", err)
+			return err
+		}
+	}
+
+	provider.loadFileConfigFromDir(configurationChan)
+
+	return nil
+}
+
+func (provider *File) loadFileConfigFromDir(configurationChan chan<- types.ConfigMessage) error {
+	files, err := ioutil.ReadDir(provider.Directory)
+
+	if err != nil {
+		log.Error(fmt.Sprintf("Unable to read Directory %s", provider.Directory), err)
+		return err
+	}
+
+	configuration := new(types.Configuration)
+	configuration.Frontends = make(map[string]*types.Frontend)
+	configuration.Backends = make(map[string]*types.Backend)
+
+	for _, file := range files {
+		if !strings.HasSuffix(file.Name(), ".toml") {
+			continue
+		}
+
+		c := provider.loadFileConfig(path.Join(provider.Directory, file.Name()), nil)
+
+		for k, v := range c.Backends {
+			configuration.Backends[k] = v
+		}
+
+		for k, v := range c.Frontends {
+			configuration.Frontends[k] = v
+		}
+	}
+
 	configurationChan <- types.ConfigMessage{
 		ProviderName:  "file",
 		Configuration: configuration,
 	}
+
 	return nil
 }
 
-func (provider *File) loadFileConfig(filename string) *types.Configuration {
+func (provider *File) loadFileConfig(filename string, configurationChan chan<- types.ConfigMessage) *types.Configuration {
 	configuration := new(types.Configuration)
 	if _, err := toml.DecodeFile(filename, configuration); err != nil {
 		log.Error("Error reading file:", err)
 		return nil
 	}
+
+	if configurationChan != nil {
+		configurationChan <- types.ConfigMessage{
+			ProviderName:  "file",
+			Configuration: configuration,
+		}
+	}
+
 	return configuration
 }
--- provider/file_test.go
+++ provider/file_test.go
@@ -1,1 +1,199 @@
 package provider
+
+import (
+	"testing"
+	"context"
+	"io/ioutil"
+	"sync"
+	"github.com/containous/traefik/safe"
+	"github.com/containous/traefik/types"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"path"
+)
+
+const (
+	frontends = `
+[frontends]
+  [frontends.frontend1]
+  backend = "backend2"
+    [frontends.frontend1.routes.test_1]
+    rule = "Host:test.localhost"
+  [frontends.frontend2]
+  backend = "backend1"
+  passHostHeader = true
+  entrypoints = ["https"] # overrides defaultEntryPoints
+    [frontends.frontend2.routes.test_1]
+    rule = "Host:{subdomain:[a-z]+}.localhost"
+  [frontends.frontend3]
+  entrypoints = ["http", "https"] # overrides defaultEntryPoints
+  backend = "backend2"
+    rule = "Path:/test"
+    `
+
+	backends = `
+[backends]
+  [backends.backend1]
+    [backends.backend1.circuitbreaker]
+      expression = "NetworkErrorRatio() > 0.5"
+    [backends.backend1.servers.server1]
+    url = "http://172.17.0.2:80"
+    weight = 10
+    [backends.backend1.servers.server2]
+    url = "http://172.17.0.3:80"
+    weight = 1
+  [backends.backend2]
+    [backends.backend2.LoadBalancer]
+      method = "drr"
+    [backends.backend2.servers.server1]
+    url = "http://172.17.0.4:80"
+    weight = 1
+    [backends.backend2.servers.server2]
+    url = "http://172.17.0.5:80"
+    weight = 2
+    `
+
+	frontend1 = `
+[frontends]
+  [frontends.frontend1]
+  backend = "backend1"
+    [frontends.frontend1.routes.test_1]
+    rule = "Host:test.localhost"
+    `
+
+	backend1 = `
+  [backends.backend1]
+    [backends.backend1.circuitbreaker]
+      expression = "NetworkErrorRatio() > 0.5"
+    [backends.backend1.servers.server1]
+    url = "http://172.17.0.2:80"
+    weight = 10
+    [backends.backend1.servers.server2]
+    url = "http://172.17.0.3:80"
+    weight = 1
+    `
+)
+
+var pool = safe.NewPool(context.Background())
+
+func createFile(t *testing.T, file string) *os.File {
+	f, err := os.Create(file)
+	if err != nil {
+		t.Error(err)
+		return nil
+	}
+	return f
+}
+
+func createTempDir(t *testing.T, dir string) string {
+	d, err := ioutil.TempDir("", dir)
+	if err != nil {
+		t.Error(err)
+		return ""
+	}
+	return d
+}
+
+func TestFileProvideSingleFile(t *testing.T) {
+	tempDir := createTempDir(t, "test")
+	tempFileName := path.Join(tempDir, "temp1.toml")
+
+	tempFile := createFile(t, tempFileName)
+	tempFile.WriteString(frontends)
+	tempFile.WriteString(backends)
+	tempFile.Close()
+
+	fileProvider := new(File)
+	c := make(chan types.ConfigMessage)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	numBackends := 2
+	numFrontends := 3
+
+	go func() {
+		for {
+			data := <-c
+			assert.Equal(t, "file", data.ProviderName)
+			assert.Equal(t, numBackends, len(data.Configuration.Backends))
+			assert.Equal(t, numFrontends, len(data.Configuration.Frontends))
+			wg.Done()
+		}
+
+	}()
+
+	fileProvider.Filename = tempFile.Name()
+	fileProvider.Watch = true
+	fileProvider.Provide(c, pool, nil)
+
+	wg.Wait()
+
+	numBackends = 1
+	numFrontends = 1
+
+	wg.Add(1)
+
+	tempFile = createFile(t, tempFileName)
+	tempFile.WriteString(frontend1)
+	tempFile.WriteString(backend1)
+
+	wg.Wait()
+
+	os.Remove(tempFile.Name())
+	os.Remove(tempDir)
+}
+
+func TestFileProvideDirectory(t *testing.T) {
+	tempDir := createTempDir(t, "test")
+
+	tempFileName1 := path.Join(tempDir, "temp1.toml")
+	tempFileName2 := path.Join(tempDir, "temp2.toml")
+
+	tempFile1 := createFile(t, tempFileName1)
+	tempFile2 := createFile(t, tempFileName2)
+	tempFile1.WriteString(frontends)
+	tempFile2.WriteString(backends)
+	tempFile1.Close()
+	tempFile2.Close()
+
+	file := new(File)
+	c := make(chan types.ConfigMessage)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	numBackends := 2
+	numFrontends := 3
+
+	go func() {
+		for {
+			data := <-c
+			assert.Equal(t, "file", data.ProviderName)
+			assert.Equal(t, numBackends, len(data.Configuration.Backends))
+			assert.Equal(t, numFrontends, len(data.Configuration.Frontends))
+			wg.Done()
+		}
+	}()
+
+	file.Directory = tempDir
+	file.Watch = true
+	file.Provide(c, pool, nil)
+
+	wg.Wait()
+
+	numBackends = 0
+	numFrontends = 0
+
+	wg.Add(1)
+
+	os.Remove(tempFile1.Name())
+	os.Remove(tempFile2.Name())
+
+	wg.Wait()
+
+	numBackends = 2
+	numFrontends = 3
+
+	os.Remove(tempDir)
+}
