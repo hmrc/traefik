0124183f3f2bf41ef4d1995150566d181239c32b
Rick <rick777@github.com>
2017-03-24 11:26:01 +0000

    Added kafka audit feed; new dependency on 'sarama'

--- middlewares/audittap/streams/kafkaSink.go
+++ middlewares/audittap/streams/kafkaSink.go
@@ -1,49 +1,63 @@
 package streams
 
-//import (
-//	. "github.com/containous/traefik/middlewares/audittap/audittypes"
-//	"log"
-//	"sync"
-//	"github.com/Shopify/sarama"
-//)
-//
-//type kafkaAuditSink struct {
-//	topic    string
-//	producer sarama.AsyncProducer
-//	join     *sync.WaitGroup
-//	render   Renderer
-//}
-//
-//func NewKafkaSink(topic, endpoint string, renderer Renderer) (sink AuditSink, err error) {
-//	config := sarama.NewConfig()
-//	config.Producer.Return.Successes = false
-//	producer, err := sarama.NewAsyncProducer([]string{endpoint}, config)
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	kas := &kafkaAuditSink{topic, producer, &sync.WaitGroup{}, renderer}
-//	kas.join.Add(1)
-//
-//	go func() {
-//		// read errors and log them, until the producer is closed
-//		for err := range producer.Errors() {
-//			log.Errorf("Kafka: %v", err)
-//		}
-//		kas.join.Done()
-//	}()
-//
-//	return kas, nil
-//}
-//
-//func (kas *kafkaAuditSink) Audit(encoded Encoded) error {
-//	message := &sarama.ProducerMessage{Topic: kas.topic, Value: encoded}
-//	kas.producer.Input() <- message
-//	return nil
-//}
-//
-//func (kas *kafkaAuditSink) Close() error {
-//	kas.producer.AsyncClose()
-//	kas.join.Wait()
-//	return nil
-//}
+import (
+	"github.com/Shopify/sarama"
+	. "github.com/containous/traefik/middlewares/audittap/audittypes"
+	"log"
+	"sync"
+)
+
+type kafkaAsyncProducer interface {
+	Input() chan<- *sarama.ProducerMessage
+	Errors() <-chan *sarama.ProducerError
+	AsyncClose()
+}
+
+type kafkaAuditSink struct {
+	topic    string
+	producer kafkaAsyncProducer
+	join     *sync.WaitGroup
+}
+
+// seam for testing
+var newAsyncProducer = func(endpoints []string, config *sarama.Config) (kafkaAsyncProducer, error) {
+	return sarama.NewAsyncProducer(endpoints, config)
+}
+
+// NewKafkaSink returns an AuditSink for sending messages to Kafka.
+func NewKafkaSink(topic string, endpoints []string) (sink AuditSink, err error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = false
+	producer, err := newAsyncProducer(endpoints, config)
+	if err != nil {
+		return nil, err
+	}
+
+	kas := &kafkaAuditSink{topic, producer, &sync.WaitGroup{}}
+	kas.logErrors()
+	return kas, nil
+}
+
+func (kas *kafkaAuditSink) logErrors() {
+	kas.join.Add(1)
+
+	go func() {
+		// read errors and log them, until the producer is closed
+		for err := range kas.producer.Errors() {
+			log.Println(err.Error())
+		}
+		kas.join.Done()
+	}()
+}
+
+func (kas *kafkaAuditSink) Audit(encoded Encoded) error {
+	message := &sarama.ProducerMessage{Topic: kas.topic, Value: encoded}
+	kas.producer.Input() <- message
+	return nil
+}
+
+func (kas *kafkaAuditSink) Close() error {
+	kas.producer.AsyncClose() // closes all the producer's goroutines and channels
+	kas.join.Wait()
+	return nil
+}
