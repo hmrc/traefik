8fc5b7ca33b672b7db66eca3410fc3712e9e3020
Todor Kolev <todor.kolev@digital.hmrc.gov.uk>
2022-03-07 17:57:33 +0000

    CAPT-68 add test ensuring events are logged on 502

--- middlewares/audittap/streams/httpSink_test.go
+++ middlewares/audittap/streams/httpSink_test.go
@@ -1,12 +1,17 @@
 package streams
 
 import (
+	"bytes"
+	"encoding/json"
+	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+
+	"github.com/containous/traefik/log"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHttpSink(t *testing.T) {
@@ -26,4 +31,30 @@ func TestHttpSink(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, string(encodedJSONSample.Bytes), got)
+}
+
+type LogEvent struct {
+	Level   string
+	Message string
+}
+
+func TestLogEventsOnNon200Response(t *testing.T) {
+
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+
+	stub := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusBadGateway)
+	}))
+	defer stub.Close()
+
+	w1, _ := NewHTTPSink("POST", stub.URL)
+	_ = w1.Audit(encodedJSONSample)
+	logEventsStr := buf.String()
+	logEvents := strings.Split(logEventsStr, "\n")
+	lastLogEvent := logEvents[len(logEvents)-2]
+	var logEvent LogEvent
+	json.Unmarshal([]byte(lastLogEvent), &logEvent)
+	assert.Equal(t, "warning", logEvent.Level)
+	assert.Equal(t, "DS_EventMissed_AuditFailureResponse audit item : [1,2,3]", logEvent.Message)
 }
